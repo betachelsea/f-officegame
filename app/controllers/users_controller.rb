6e7ffb276283c3982cb68b5a64b6338f09b9bcd4
beta_chelsea <beta.chelsea@gmail.com>
2013-10-14 17:23:16 +0900

    user一覧API追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
+# coding: utf-8
+#
 class UsersController < ApplicationController
+
+    def index
+        @user = User.order("id")
+        respond_to do |format|
+            format.json { render :json => @user }
+        end
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def create_json(data)
+        data = [data] unless data.class == Array
+        data = data.inject([]){ |arr, obj|
+            arr << {
+                :id => obj.id,
+                :created_at => obj.created_at
+            }; arr
+        }
+        { :data => data }.to_json
+    end
+
 end
