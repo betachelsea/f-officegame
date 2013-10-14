442fb646ba5b61879d610410d819ffb339f053e0
beta_chelsea <beta.chelsea@gmail.com>
2013-10-14 16:59:02 +0900

    Userオブジェクト作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+
+0.upto(9) do |idx|
+    User.create()
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
