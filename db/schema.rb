60bc641d6751b17cece256decb54c59aa90ca374
beta_chelsea <beta.chelsea@gmail.com>
2013-10-16 08:52:34 +0900

    boardモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131014074311) do
+ActiveRecord::Schema.define(version: 20131015235124) do
+
+  create_table "boards", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "session_id", null: false
