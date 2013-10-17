2ce26b6a0a80d8d8f187e4c041688362b98ee2e6
beta_chelsea <beta.chelsea@gmail.com>
2013-10-17 20:41:37 +0900

    プレイヤー2名のidをちゃんと保持できるよう修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,11 +1,12 @@
 class BoardsController < ApplicationController
 
     def create
+        @user = User.find_by_session_id(cookies[:sid])
         @board = Board.new(
             state: "{'set'}",
             turn: 1,
-            player1: 3,
-            player2: params[:user_id],
+            player1: @user.id,
+            player2: params[:opponent_id],
             player1_stone_count: 0,
             player2_stone_count: 0
         )
