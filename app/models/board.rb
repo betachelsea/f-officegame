54900cc83053dda6a24e07ba96f029ff22ffebe7
beta_chelsea <beta.chelsea@gmail.com>
2013-10-20 01:14:53 +0900

    有効な位置への一手か確認処理を追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,18 +2,30 @@ class Board < ActiveRecord::Base
 
     class << self
         def update_state(state, x, y, player)
-            if (player == -1)
-                return false #有効な更新ではないため中止
-            end
             
-            #隣り合うセルに石が存在しない場合は不可
-            
-            #隣り合うセルに敵石が存在しない場合は不可
+            #有効な更新ではないため中止
+            return false if (player == -1)
+            return false if !effective_move(state, x, y, player)
             
             #ひっくり返せる石があった場合ひっくり返す処理を実施
             
             state[x][y] = player
             true #更新有効
+        end
+
+        def effective_move(state, x, y, player)
+            enemy = player == 1 ? 2 : 1
+            (-1..1).each do |x_diff|
+                next if (x+x_diff < 0 || 7 < x+x_diff)
+                (-1..1).each do |y_diff|
+                    next if (y+y_diff < 0 || 7 < y+y_diff)
+
+                    if (enemy == state[x+x_diff][y+y_diff])
+                        return true
+                    end
+                end
+            end
+            false #無効な一手（周囲のセルが反転対象外
         end
     end
 
