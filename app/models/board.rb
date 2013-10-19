b647fe2a838824ede7c2f53dc97f1016c1636483
beta_chelsea <beta.chelsea@gmail.com>
2013-10-19 23:04:32 +0900

    処理順確認コメントを追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,6 +5,13 @@ class Board < ActiveRecord::Base
             if (player == -1)
                 return false #有効な更新ではないため中止
             end
+            
+            #隣り合うセルに石が存在しない場合は不可
+            
+            #隣り合うセルに敵石が存在しない場合は不可
+            
+            #ひっくり返せる石があった場合ひっくり返す処理を実施
+            
             state[x][y] = player
             true #更新有効
         end
