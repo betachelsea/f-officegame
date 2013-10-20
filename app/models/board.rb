e5b5f5886823db2c78c86e8451fa9e4aeac9ef60
beta_chelsea <beta.chelsea@gmail.com>
2013-10-20 12:12:16 +0900

    石をひっくり返す仕組みを実装

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,12 +5,45 @@ class Board < ActiveRecord::Base
             
             #有効な更新ではないため中止
             return false if (player == -1)
-            return false if !effective_move(state, x, y, player)
-            
-            #ひっくり返せる石があった場合ひっくり返す処理を実施
-            
-            state[x][y] = player
-            true #更新有効
+            return false if (state[x][y] != 0)
+
+            #return false if !effective_move(state, x, y, player)
+            effective = false
+            enemy = player == 1 ? 2 : 1
+            (-1..1).each do |x_diff|
+                next if (x+x_diff < 0 || 7 < x+x_diff)
+                (-1..1).each do |y_diff|
+                    next if (y+y_diff < 0 || 7 < y+y_diff)
+                    if (enemy == state[x+x_diff][y+y_diff])
+                        if (reverse?(state, x+x_diff, x_diff, y+y_diff, y_diff, player, enemy))
+                            continuous_reverse(state, x+x_diff, x_diff, y+y_diff, y_diff, player, enemy)
+                            #state[x][y]=player
+                            effective = true
+                        end
+                    end
+                end
+            end
+            state[x][y]=player if effective
+            effective
+            #state[x][y] = player
+        end
+
+        def continuous_reverse(state, x, x_diff, y, y_diff, player, enemy)
+            if (state[x][y] == enemy)
+                state[x][y] = player
+                continuous_reverse(state, x+x_diff, x_diff, y+y_diff, y_diff, player, enemy)
+            end
+        end
+
+        def reverse?(state, x, x_diff, y, y_diff, player, enemy)
+            check_stone = state[x][y]
+            if (check_stone == player)
+                true
+            elsif (check_stone == enemy)
+                reverse?(state, x+x_diff, x_diff, y+y_diff, y_diff, player, enemy)
+            else
+                false
+            end
         end
 
         def effective_move(state, x, y, player)
